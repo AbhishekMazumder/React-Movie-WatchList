a511e45edda1a5326eccee7d32fb999ebfa58924
abhishek <abhishekmazumder69@gmail.com>
2021-04-02 11:03:00 +0530

    Changed to HashRouter as Router from BrowserRouter to solve deployment issue.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import WatchList from './components/WatchList';
 import Watched from './components/Watched';
